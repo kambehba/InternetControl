fde14ae9332f610c6dc08e2f01439e74245737ea
kambehba <kamparsen@yahoo.com>
2017-03-07 12:28:44 -0500

    desktop app

---
+++ internet-control-desktop/InternetControl/InternetControl/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InternetControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static System.Windows.Threading.DispatcherTimer mainTimer; 
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainTimer = new System.Windows.Threading.DispatcherTimer();
+            mainTimer.Tick += new EventHandler(OnTimedEvent);
+            mainTimer.Interval = TimeSpan.FromSeconds(1);
+            light.Background = Brushes.Red;
+            lightstatus.Content = "OFF";
+          
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            mainTimer.Start();
+
+         
+
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            mainTimer.Stop();
+
+           
+
+        }
+
+
+        private void OnTimedEvent(object source, EventArgs e)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://localhost:3000/api/outputs");
+            var response = (HttpWebResponse)request.GetResponse();
+
+            
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            var g = responseString.Split('"');
+            
+            if(g[7] == "ON")
+            {
+
+                light.Background = Brushes.Green;
+                lightstatus.Content = "ON";
+            }
+
+            if (g[7] == "OFF")
+            {
+
+                light.Background = Brushes.Red;
+                lightstatus.Content = "OFF";
+            }
+          
+            
+        }
+    }
+}
